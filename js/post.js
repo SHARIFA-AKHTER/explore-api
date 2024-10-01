e1f23ce46df1271c029d91e72bdfee172d54c5c0
unknown <sr0589071@gmail.com>
2024-10-01 15:18:32 +0600

    first commit

---
+++ js/post.js
@@ -0,0 +1,34 @@
+// function loadPosts(){
+//     console.log('loading posts data')
+// }
+// loadPosts();
+
+function loadPosts(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => displayPosts(data))
+}
+
+/*
+1. get the container element where you want to add the new elements
+*/
+
+function displayPosts(posts){
+    const postsContainer = document.getElementById('posts-container');
+    for(const post of posts){
+        const postDiv = document.createElement('div');
+        postDiv.classList.add('post')
+        console.log(post);
+        postDiv.innerHTML = `
+        <h4>User-${post.userId}</h4>
+        <5>Post: ${post.title}</h5>
+        <p>Post Description: ${post.body}</p>
+
+        `;
+        postsContainer.appendChild(postDiv);
+
+    }
+}
+
+loadPosts();
+
